1faea414193d06650207f42efe363de40ff593f2
Aron <aronwht@gmail.com>
2015-09-23 09:57:11 +0200

    add 1 !

--- Git-test-project/src/ua/awc/tset/Main.java
+++ Git-test-project/src/ua/awc/tset/Main.java
@@ -2,7 +2,7 @@ package ua.awc.tset;
 
 public class Main {
 	public static void main(String [] args){
-		System.out.println("Hellow from Git Hub!");
+		System.out.println("Hellow from Git Hub! ! ");
 	}
 
 }
